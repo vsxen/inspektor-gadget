29bd94601f2d4e99f33e758394e2a221bb0d7a20
Mauricio Vásquez <mauriciov@microsoft.com>
2023-03-13 14:56:38 -0500

    gadgets: Add bpftrace initial integration
    The initial integration of bpftrace in Inspektor Gadget consists of
    executing the bpftrace binary on the gadget pod and streaming its
    output to the client.
    
    The current integration does not support filtering events by
    namespace/pod/container nor enriching them with that information. This
    is planned to be done in the second phase of this work.

--- gadget-container/gadgettracermanager/main.go
+++ gadget-container/gadgettracermanager/main.go
@@ -39,6 +39,9 @@ import (
 	gadgetservice "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgettracermanager"
 	pb "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgettracermanager/api"
+
+	// TODO: remove once we import all-gadgets
+	_ "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/bpftrace"
 )
 
 var (
